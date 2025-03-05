4aa6b9d06be8b5d84954f3f4d856c0c698aba7a3
Aspy2025 <lalejandro.2005martinez@gmail.com>
2025-03-05 13:16:14 -0500

    Primer commit

---
+++ src/main/java/com/example/demo/MicacharritoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicacharritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicacharritoApplication.class, args);
+	}
+
+}
