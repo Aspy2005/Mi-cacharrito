b342b00b0262e43079478802d7cb34e4a02072ab
Aspy2025 <lalejandro.2005martinez@gmail.com>
2025-03-06 21:23:23 -0500

    Constructor, Getters y setters

--- src/main/java/com/example/demo/modelo/Credencialesusuario.java
+++ src/main/java/com/example/demo/modelo/Credencialesusuario.java
@@ -10,11 +10,48 @@ public class Credencialesusuario {
 	@Id
 	@Column(name="idcredencial")
 	private Long idcredencial; 
-	
+
 	@Column(name="contraseña")
 	private String contraseña;
 	
 	@OneToOne
 	@JoinColumn(name = "identificacion") 
 	private Usuario usuario;
+
+	public Credencialesusuario() {
+		super();
+	}
+
+	public Credencialesusuario(Long idcredencial, String contraseña, Usuario usuario) {
+		super();
+		this.idcredencial = idcredencial;
+		this.contraseña = contraseña;
+		this.usuario = usuario;
+	}
+
+	public Long getIdcredencial() {
+		return idcredencial;
+	}
+
+	public void setIdcredencial(Long idcredencial) {
+		this.idcredencial = idcredencial;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
 }
