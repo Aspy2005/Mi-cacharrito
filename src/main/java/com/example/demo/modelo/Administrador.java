3bef508d5cae5f63b27288f1a93e85b2e799982b
ASUS <ASUS@DESKTOP-H8BP8UG>
2025-03-06 20:53:25 -0500

    creacion tablas

--- src/main/java/com/example/demo/modelo/Administrador.java
+++ src/main/java/com/example/demo/modelo/Administrador.java
@@ -1,5 +1,24 @@
 package com.example.demo.modelo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "administrador")
 public class Administrador {
+	@Id
+	@Column(name="identificacionadmi")
+	private Long identificacionadmi; 
+	
+	@Column(name="usuario")
+	private String usuario;
+	
+	@Column(name="contraseña")
+	private String contraseña; 
+	
+	
+	
 
 }
--- src/main/java/com/example/demo/modelo/Usuario.java
+++ src/main/java/com/example/demo/modelo/Usuario.java
@@ -1,5 +1,7 @@
 package com.example.demo.modelo;
 
+import java.sql.Date;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,5 +11,25 @@ public class Usuario {
 	@Id
 	@Column(name="identificacion")
 	private Long identificacion; 
+	
+	@Column(name="nombre")
+	private long nombre;
+	
+	@Column(name="fechalince")
+	private Date fechalince;
+	
+	@Column(name="categoria")
+	private String categoria;
+	
+	@Column(name="vigencia")
+	private Date vigencia;
+	
+	
+	@Column(name="telefono")
+	private Date telefono;
+	
+	@Column(name="email")
+	private String email;
+	
 	
 }
