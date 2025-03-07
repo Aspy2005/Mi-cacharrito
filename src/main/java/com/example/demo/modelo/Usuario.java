3a3284a0d38ab881e510bc115392bc460ad9d669
ASUS <ASUS@DESKTOP-H8BP8UG>
2025-03-06 20:11:47 -0500

    Creacion tabla

--- src/main/java/com/example/demo/modelo/Usuario.java
+++ src/main/java/com/example/demo/modelo/Usuario.java
@@ -1,5 +1,13 @@
 package com.example.demo.modelo;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 
+	@Id
+	@Column(name="identificacion")
+	private Long identificacion; 
+	
 }
