3d82e57df9c10453cceb28ab965fea7aa071b314
Aspy2025 <lalejandro.2005martinez@gmail.com>
2025-03-09 16:36:32 -0500

    Controlador usuario

--- src/main/java/com/example/demo/controlador/UsuarioControlador.java
+++ src/main/java/com/example/demo/controlador/UsuarioControlador.java
@@ -1,5 +1,25 @@
-package com.example.demo.controlador;
+	package com.example.demo.controlador;
 
-public class UsuarioControlador {
+	import java.util.List;
 
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.web.bind.annotation.*;
+
+	import com.example.demo.repositorio.UsuarioRepositorio;
+	import com.example.demo.modelo.Usuario;
+
+
+	@RestController
+	@RequestMapping("/ver/usuario")
+	@CrossOrigin(origins = "http://localhost:4200/")
+	public class UsuarioControlador {
+
+		
+		@Autowired
+		private UsuarioRepositorio repositorio;
+		
+		@GetMapping("/lista")
+	    public List<Usuario> listaEmpleados() {
+	        return repositorio.findAll();
+	    }
 }
