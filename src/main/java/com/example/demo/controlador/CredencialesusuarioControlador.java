3d248fcbff176372a19436bf19c3880b17189423
carlos42425 <cf225213@gmail.com>
2025-03-09 17:19:39 -0500

    controladores

--- src/main/java/com/example/demo/controlador/CredencialesusuarioControlador.java
+++ src/main/java/com/example/demo/controlador/CredencialesusuarioControlador.java
@@ -1,5 +1,30 @@
 package com.example.demo.controlador;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.modelo.Credencialesusuario;
+import com.example.demo.repositorio.CredencialesusuarioRepositorio;
+
+	@RestController
+	@RequestMapping("/ver/credencialesusuario")
+	@CrossOrigin(origins = "http://localhost:4200/")
 public class CredencialesusuarioControlador {
+	
+
+		
+		@Autowired
+		private CredencialesusuarioRepositorio repositorio;
+		
+		@GetMapping("/lista")
+	    public List<Credencialesusuario> listaEmpleados() {
+	        return repositorio.findAll();
 
 }
+	}
+
--- src/main/java/com/example/demo/controlador/VehiculoControlador.java
+++ src/main/java/com/example/demo/controlador/VehiculoControlador.java
@@ -1,5 +1,31 @@
 package com.example.demo.controlador;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.modelo.Vehiculo;
+
+import com.example.demo.repositorio.VehiculoRepositorio;
+
+
+	@RestController
+	@RequestMapping("/ver/vehiculo")
+	@CrossOrigin(origins = "http://localhost:4200/")
 
 public class VehiculoControlador {
 
+		@Autowired
+		private VehiculoRepositorio repositorio;
+		
+		@GetMapping("/lista")
+	    public List<Vehiculo> listaEmpleados() {
+	        return repositorio.findAll();
+	    }
 }
+
+
